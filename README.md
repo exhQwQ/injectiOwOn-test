1c0ffee53a631f8b2746b81a4b8d2b95a50ef30d
Riven Skaye <riven@tae.moe>
2023-01-23 13:12:56 +0100

    A few small edits

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # injectiOwOn  
-vendy is a big meanie
+vendy is a big meanie x2
 
 # HOW TO USE
-since im lazy, this thing only supports discord stable and linux (if you like this thing, tell me and ill add suppoprt)  
-run this thing:  
+This is a rather naïve CLI installer and as such it isn't perfect yet.
+That said, it might work, so try running the following commands and report back to us:  
 `git clone https://github.com/Vendicated/Vencord && cd Vencord; git clone https://github.com/exhq/injectiOwOn && cd injectiOwOn; sudo python main.py`
+
+# Adding platform support
+Just send in a PR that adds a platform-specific file and make the proper changes
+to `__main__.py`. Just ensure all of the logic happens in a function called `get_discord`
+that takes one optional string argument that matches a release channel.
+Look at the Windows and Linux code for reference material.  
+Alternatively, let ECHO know what to add/fix in the Vencord server!
---
+++ injectiowon/__main__.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import argparse
+import errno
+import os
+import sys
+
+if "linux" in sys.platform:
+    from linux import get_discord
+elif "win" in sys.platform:
+    from windows import get_discord
+else:
+    print(f"Sorry, {sys.platform} is not yet supported!")
+    exit(1)
+
+parser = argparse.ArgumentParser(
+    prog="InjectiOwOn",
+    description=("Makes a piss poor attempt at being a CLI Vencord installer, "
+                 "but doesn't do so in the Devil's tongue (Go)"),
+    epilog="Proudly presented by ECHO & Riven Skaye"
+)
+parser.add_argument(
+    "channel",
+    required=False,
+    default="stable",
+    choices=["stable", "canary", "dev", "devel", "development", "ptb"],
+    type=str.lower,
+    help="Optional release channel to install for."
+)
+# @ECHO please do add a short and long option pair for people to specify a
+# custom path. It should point to the resources folder. Symlinks and
+# relative paths should be safe, so you'll need to edit some code to handle it.
+# parser.add_argument("-p", "--installdir", ...)
+
+
+def main() -> None:
+    print("welcome to InjectiOwOn! Sit back and I'll do everything :3")
+
+    discordfolder: Path = Path(".")
+    vencord_base_path = Path(__file__).parent.parent.resolve().absolute()
+
+    dist_path = vencord_base_path / "dist"
+    if not dist_path.exists():
+        print("bitchass, run `pnpm i` and `pnpm build` first")
+        exit()
+
+    try:
+        discordfolder = get_discord(args.channel)
+    except FileNotFoundError as fnfe:
+        print(f"{fnfe.strerror}\n{fnfe.errno}: {os.strerror(fnfe.errno)}")
+
+    for child in discordfolder.iterdir():
+        if child.name.lower() == "resources":
+            discordfolder = child
+            break
+        elif child.name.lower().startswith("app-"):
+            for grandchild in child.iterdir():
+                if grandchild.name.lower() == "resources":
+                    discordfolder = grandchild
+                    break
+        elif child.name.startswith("Discord"):
+            for grandchild in child.iterdir():
+                if grandchild.name.lower() == "resources":
+                    discordfolder = grandchild
+                    break
+                elif child.name.lower().startswith("app-"):
+                    for greatgrandchild in child.iterdir():
+                        if greatgrandchild.name.lower() == "resources":
+                            discordfolder = greatgrandchild
+                            break
+    else:
+        raise OSError(errno.ENOENT,
+                      "Proper folder not found, please start Discord once!",
+                      str(discordfolder))
+
+    (discordfolder / "app").mkdir(parents=False, exist_ok=True)
+    appfolder = discordfolder/"app"
+    (appfolder/"package.json").touch(exist_ok=True)
+    (appfolder/"index.js").touch(exist_ok=True)
+
+    with open(appfolder/"package.json", "w+") as pkg:
+        pkg.write('{ "name": "discord", "main": "index.js" }\n')
+    with open(appfolder/"index.js", "w+") as idx:
+        idx.write(f"require({dist_path/'patcher.js'});\n"
+                  "require(../app.asar);")
+
+    print("That should be all. Please fully close Discord and restart to check"
+          " if it worked, your settings should contain a Vencord listing!")
+
+
+args = parser.parse_args()
+if __name__ == "__main__":
+    main()
